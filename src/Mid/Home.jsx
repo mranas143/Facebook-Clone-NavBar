f99d7fd6f95be624e9d22a7864dcc749f90b8826
AnasRaza <mr.anas.raza341@gmail.com>
2026-07-03 07:09:27 -0700

    Initial commit

--- src/Mid/Home.jsx
+++ src/Mid/Home.jsx
@@ -3,40 +3,12 @@ import React from 'react'
 function Home() {
   return (
     <>
-    <div style={{display:"flex",justifyContent:"center",alignItems:"center",}}>
-        <h1>Home</h1>
-    </div>
-    <div style={{display:"flex",justifyContent:"center",alignItems:"center",}}>
-            <h1>Home</h1>
-        </div>
-    <div style={{display:"flex",justifyContent:"center",alignItems:"center",}}>
-            <h1>Home</h1>
-        </div>
-    <div style={{display:"flex",justifyContent:"center",alignItems:"center",}}>
-            <h1>Home</h1>
-        </div>
-    <div style={{display:"flex",justifyContent:"center",alignItems:"center",}}>
-            <h1>Home</h1>
-        </div>
-    <div style={{display:"flex",justifyContent:"center",alignItems:"center",}}>
-            <h1>Home</h1>
-        </div>
-    <div style={{display:"flex",justifyContent:"center",alignItems:"center",}}>
-            <h1>Home</h1>
-        </div>
-    <div style={{display:"flex",justifyContent:"center",alignItems:"center",}}>
-            <h1>Home</h1>
-        </div>
-    <div style={{display:"flex",justifyContent:"center",alignItems:"center",}}>
-            <h1>Home</h1>
-        </div>
-    <div style={{display:"flex",justifyContent:"center",alignItems:"center",}}>
-            <h1>Home</h1>
-        </div>
-    <div style={{display:"flex",justifyContent:"center",alignItems:"center",}}>
+    
+    <div style={{display:"flex",justifyContent:"center",alignItems:"center",fontSize:"50px",height:"100vh"}}>
             <h1>Home</h1>
         </div></>
- )
+ 
+)
 }
 
 export default Home
